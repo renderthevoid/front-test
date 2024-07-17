17561c9b328a32fa3d1585d8e849279145c73e54
Vlad <sylaristneskyrim@gmail.com>
2024-07-17 12:14:45 +0500

    some html

---
+++ src/components/GridElement.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="element">
+    <div class="element__wrapper">
+      <div class="element__image">
+        <img :src="props.img" alt="">
+      </div>
+      <div class="element__counter">
+        <div class="element__counter-item">4</div>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IGridElementProps {
+  id?: number
+  title?: string
+  img?: string
+}
+const props = defineProps<IGridElementProps>()
+</script>
+
+<style scoped lang="scss">
+@import '../assets/main.scss';
+.element {
+  position: relative;
+  background-color: $color-background-items;
+  border: 1px solid $color-border;
+
+  -webkit-user-select: none; /* Chrome/Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE/Edge */
+  user-select: none; /* Стандартный синтаксис */
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 24px;
+  }
+
+  &__image {
+  }
+
+  &__counter {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 4px 8px;
+    border: 1px solid $color-border;
+    border-radius: 6px 0 0 0;
+  }
+
+  &__counter-item {
+    font-weight: 500;
+    font-size: 10px;
+    text-align: center;
+    color: #fff;
+    opacity: 0.4;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,129 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script setup lang="ts">
+import GridElement from '@/components/GridElement.vue'
+import { onMounted, ref } from 'vue'
+
+const dragItem = ref<any>()
+interface IDragItem {
+  id?: number
+  img?: string
+}
+const items = ref<IDragItem[]>([
+  { id: 0, img: '/src/components/icons/GreenIcon.svg' },
+  { id: 1, img: '/src/components/icons/YellowIcon.svg' },
+  { id: 2, img: '/src/components/icons/PurpleIcon.svg' }
+])
 
 </script>
 
-<style scoped>
+<template>
+  <main>
+    <div class="main">
+      <div class="container">
+        <div class="content">
+          <div class="left">
+            <div class="left__wrapper">
+              <div class="left__top">
+                <img src="../assets/image.svg" alt="" />
+              </div>
+              <div class="left__content">
+                <div class="left__line left__line_wide loading"></div>
+                <div
+                  class="left__line loading"
+                  v-for="i in 5"
+                  :key="i"
+                  :style="{ width: `${Math.floor(Math.random() * 200)}px` }"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div class="drop-zone">
+            <grid-element
+              v-for="(item, index) in items"
+              :key="item.id"
+              :img="item.img"
+              @dragstart="handleDragStart(index)"
+              @dragend="handleDragEnd"
+              @dragover.prevent
+              @drop="handleDrop(index)"
+            >
+            </grid-element>
+            <!-- <div
+              class="drop-el"
+              v-for="(item, index) in items"
+              :key="item.id"
+              :draggable="Object.keys(item).length === 0 ? false : true"
+              @dragstart="handleDragStart(index)"
+              @dragend="handleDragEnd"
+              @dragover.prevent
+              @drop="handleDrop(index)"
+            >
+              {{ item.title }}
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
+<style scoped lang="scss">
+@import '../assets/main.scss';
+#app {
+  color: black;
+  margin-top: 60px;
+}
+.left {
+  background-color: $color-background-items;
+  border: 1px solid $color-border;
+  border-radius: 12px;
+  padding: 24px;
+  &__wrapper {
+  }
+
+  &__top {
+    margin: 0 0 24px;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+  }
+
+  &__line {
+    width: 155px;
+    height: 10px;
+    border-radius: 10px;
+    &_wide {
+      width: 190px;
+      height: 26px;
+    }
+  }
+}
+.loading {
+}
+
+.drop-zone {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  border: 1px solid #4d4d4d;
+  border-radius: 12px;
+  overflow: hidden;
+  background-color: $color-background-items;
+  min-height: 100px;
+}
+
+.drop-el {
+  background-color: #eee;
+  border: 1px solid gray;
+}
+
+.content {
+  margin-top: 100px;
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  gap: 40px;
+}
 </style>
